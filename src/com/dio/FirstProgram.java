8d1f1d1b8dc9f47762ab9dafb8a74230a068742c
Arcanjo <amanda.r.arcanjo@gmail.com>
2022-05-06 08:31:09 -0300

    another try

--- src/com/dio/FirstProgram.java
+++ src/com/dio/FirstProgram.java
@@ -7,9 +7,7 @@ public class FirstProgram {
 	public static void main(String[] args) {
 		Cat_Model cat = new Cat_Model();
 		Books books = new Books();
-		/* int a = 5;
-		int b = 15;
-		System.out.println("Hello World " + (a+b)); */
+		
 		System.out.println(cat);
 		System.out.println(books);
 	}
